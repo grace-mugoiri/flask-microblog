7f4f6d3c1ede85c4c160c2d3b42d2fc6b8416683
grace-mugoiri <gracemugoiri@gmail.com>
2021-01-13 14:44:20 +0300

    templating

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
 """import modules"""
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello world"
+    """index function"""
+    user = {'username': 'Mugoiri'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in portland!'
+        },
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The Avengers movie was so cool'
+		}
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)

9688059b3eb12053e84bfa1465a461b8fa532c9d
grace-mugoiri <gracemugoiri@gmail.com>
2021-01-18 12:38:42 +0300

    user loader function and UserMixin

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 """import modules"""
 from datetime import datetime
 from app import db
+from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     """add class variables"""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -16,10 +18,18 @@ class User(db.Model):
         return '<User {}>'.format(self.username)
 
     def set_password(self, password):
+		"""setpassword"""
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
+		"""checkpassword"""
         return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+	"""loaduser"""
+	return User.query.get(int(id))
 
 
 class Post(db.Model):

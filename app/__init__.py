3ca1931c9656401c01154267528b8e7e2d5cc1c0
grace-mugoiri <gracemugoiri@gmail.com>
2021-01-25 15:37:25 +0300

    boostraping

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
--- app/email.py
+++ app/email.py
@@ -26,11 +26,11 @@ def send_password_reset_email(user):
 def send_async_email(app, msg):
     """asyncemail"""
     with app.app_context():
-    mail.send(msg)
+        mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_emaill, args=(app, msg)).start()
+    Thread(target=send_async_email, args=(app, msg)).start()

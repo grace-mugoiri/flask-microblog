c4c92f4e6bacdcd606dec20b0aa40ddd4cea8bdb
grace-mugoiri <gracemugoiri@gmail.com>
2021-01-25 15:45:48 +0300

    dates and time

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 
 app = Flask(__name__)
@@ -18,6 +19,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
